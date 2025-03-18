e6df63b7687e582e87e829c227412718b0cf60d5
AMN NAYEEF <nayeefamn@gmail.com>
2025-03-18 17:07:39 +0600

    logical operators

---
+++ script.js
@@ -0,0 +1,74 @@
+/*let js = 'amazing'
+console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "BOB";
+console.log(firstName)
+
+
+let country = 'Bangladesh';
+let continent = 'Asia';
+let population = 16;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+
+true;
+console.log(true);
+
+let javscriptIsFun = true;
+console.log(javscriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javscriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'Jonas');*/
+
+// let afe = 30;
+// age = 31;
+
+// const birthYear = 1991;
+
+// var job = 'programmer';
+// job = 'teacher'; 
+
+
+// // type conversion
+// const inputYear = '1991';
+// console.log(inputYear);
+// console.log(Number(inputYear), inputYear);
+// console.log(Number(inputYear) + 19);
+// console.log(Number('Nayeef'));
+// console.log(typeof NaN);
+
+// console.log(String(23), 23);
+
+
+// // type coercion
+
+// console.log('I am ' + 23 + ' Years old');
+// console.log('23' - '10' - 3); // in js + sign means concate for strings
+
+
+// let n = '1' + 1;
+// n = n - 1;
+// console.log(n);
+
+
+const hasDriverLicense = true;
+const hasGoodVision = true;
+console.log(hasDriverLicense && hasGoodVision);
+console.log(hasDriverLicense || hasGoodVision);
+console.log(!hasDriverLicense);
+
+const shouldDrive = hasDriverLicense && hasGoodVision;
+
+if (shouldDrive) {
+    console.log('Sarah is able to drive');
+} else {
+    console.log('Someone else should Drive....');
+}
